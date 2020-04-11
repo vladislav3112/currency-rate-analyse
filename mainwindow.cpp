f29ca97b55784242de12366f80d0c85545666299
dyushakolesov <dyushakolesov@mail.ru>
2020-04-11 20:33:10 +0300

    plot and scalable ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,17 +9,46 @@
 #include <QtNetwork/QNetworkReply>
 #include <QDebug>
 #include <qwt_plot.h>
+#include <qwt_plot_grid.h>
+#include <qwt_legend.h>
+#include <qwt_plot_curve.h>
+#include <qwt_symbol.h>
+#include <qwt_plot_picker.h>
+#include <qwt_picker_machine.h>
+//add
+#include <qwt_plot_magnifier.h>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
     ui->dateEdit->setDate(QDate::currentDate());
     ui->dateEdit_2->setDate(QDate::currentDate());
     ui->comboBox->addItem("доллар");
     ui->comboBox->addItem("евро");
     currency_code.insert("R01235","доллар");
     currency_code.insert("R01239","евро");
+
+    ui->qwtPlot->setTitle( "Курсы валют" );
+    ui->qwtPlot->setAxisTitle(QwtPlot::yLeft, "Стоимомть");
+    ui->qwtPlot->setAxisTitle(QwtPlot::xBottom, "Дни");
+    ui->qwtPlot->insertLegend(new QwtLegend());
+    // Включить сетку
+    QwtPlotGrid *grid = new QwtPlotGrid(); //
+    grid->setMajorPen(QPen( Qt::gray, 2 )); // цвет линий и толщина
+    grid->attach(ui->qwtPlot); // добавить сетку к полю графика
+    //picker_settings
+    QwtPlotPicker *d_picker = new QwtPlotPicker(QwtPlot::xBottom, QwtPlot::yLeft,QwtPlotPicker::CrossRubberBand,
+        QwtPicker::ActiveOnly, // включение/выключение
+    ui->qwtPlot->canvas() ); // ассоциация с полем
+    // Цвет перпендикулярных линий
+    d_picker->setRubberBandPen( QColor( Qt::red ) );
+    // цвет координат положения указателя
+    d_picker->setTrackerPen( QColor( Qt::black ) );
+    // непосредственное включение вышеописанных функций
+    d_picker->setStateMachine(new QwtPickerDragPointMachine());
+
 }
 
 MainWindow::~MainWindow()
@@ -101,6 +130,28 @@ void MainWindow::xml_parse(){
 }
     file->close();
 
-    if(currency_rate.isEmpty()) ui->label_curr_rate->setText("В выходной день данных нет");
-    else        ui->label_curr_rate->setText(QString::number(currency_rate[0]));
+    addNewCurve(ui->comboBox->currentText(),currency_rate);
 }
+
+void MainWindow::addNewCurve(QString currency_name, QVector<double> currency_rate){//add color chooser
+    QwtPlotCurve *curve = new QwtPlotCurve();
+        curve->setTitle(currency_name);
+        curve->setPen( Qt::blue, 6 ); // цвет и толщина кривой
+        curve->setRenderHint( QwtPlotItem::RenderAntialiased, true ); // сглаживание
+
+        // Маркеры кривой
+        QwtSymbol *symbol = new QwtSymbol( QwtSymbol::Ellipse,
+            QBrush( Qt::yellow ), QPen( Qt::red, 2 ), QSize( 8, 8 ) );
+        curve->setSymbol( symbol );
+
+        // Добавить точки на ранее созданную кривую
+        QPolygonF points;
+
+         for(int i = 0;i < currency_rate.size(); i++){
+            points << QPointF( i, currency_rate[i]);
+         }
+
+         curve->setSamples( points ); // ассоциировать набор точек с кривой
+         curve->attach(ui->qwtPlot);
+         ui->qwtPlot->replot();
+};
